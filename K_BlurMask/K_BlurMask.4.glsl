1480b96ba7244314cc32e1e8da3779a5716c7a7f
kmatchbox <kyle.obley@gmail.com>
2014-03-24 18:02:56 +0100

    Old pass 2 pushed to 4

--- K_BlurMask/K_BlurMask.4.glsl
+++ K_BlurMask/K_BlurMask.4.glsl
@@ -2,19 +2,44 @@
 // K_BlurMask v1.1
 // Shader written by:   Kyle Obley (kyle.obley@gmail.com)
 //
-// Pass #4: Put everything back together
+// Pass #4: Horizontal blur
 //
 
-uniform sampler2D adsk_results_pass3, front, matte;
-uniform float adsk_result_w, adsk_result_h;
+uniform sampler2D adsk_results_pass3;
+uniform float adsk_result_w, adsk_result_h, sigma, h_bias;
+const float pi = 3.141592653589793238462643383279502884197969;
 
 void main() {
-	vec2 iResolution = vec2(adsk_result_w, adsk_result_h);
-	vec2 uv = (gl_FragCoord.xy/iResolution.xy);
-	
-	vec4 blurred = texture2D(adsk_results_pass3, uv).rgba;
-	vec3 back = texture2D(front, uv).rgb;
-	vec3 src_matte = texture2D(matte, uv).rgb;
-	
-	gl_FragColor = vec4 (mix(back, (blurred.rgb/(blurred.a + 0.0001)), src_matte.r), src_matte.r);
+	vec2 xy = gl_FragCoord.xy;
+	vec2 px = vec2(1.0) / vec2(adsk_result_w, adsk_result_h);
+
+	float h_sigma = sigma * h_bias;
+
+	int support = int(h_sigma * 3.0);
+
+	// Incremental coefficient calculation setup as per GPU Gems 3
+	vec3 g;
+	g.x = 1.0 / (sqrt(2.0 * pi) * h_sigma);
+	g.y = exp(-0.5 / (h_sigma * h_sigma));
+	g.z = g.y * g.y;
+
+	if(h_sigma == 0.0) {
+		g.x = 1.0;
+	}
+
+	// Centre sample
+	vec4 a = g.x * texture2D(adsk_results_pass3, xy * px);
+	float energy = g.x;
+	g.xy *= g.yz;
+
+	// The rest
+	for(int i = 1; i <= support; i++) {
+		a += g.x * texture2D(adsk_results_pass3, (xy - vec2(float(i), 0.0)) * px);
+		a += g.x * texture2D(adsk_results_pass3, (xy + vec2(float(i), 0.0)) * px);
+		energy += 2.0 * g.x;
+		g.xy *= g.yz;
+	}
+	a /= energy;
+
+	gl_FragColor = a;
 }
