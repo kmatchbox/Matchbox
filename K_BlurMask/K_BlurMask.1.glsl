b019e908517240ec3571072fa38d1a14b5b3ba27
kmatchbox <kyle.obley@gmail.com>
2014-03-24 18:00:00 +0100

    Dummy pass to send the front along to the last pass so Flame won't ask for 4 inputs.

--- K_BlurMask/K_BlurMask.1.glsl
+++ K_BlurMask/K_BlurMask.1.glsl
@@ -2,11 +2,11 @@
 // K_BlurMask v1.1
 // Shader written by:   Kyle Obley (kyle.obley@gmail.com)
 //
-// Pass #1: Multiply front by the matte
+// Pass #1: Pass front along to the other shaders to get around the way things are implimented
 //
 
 
-uniform sampler2D front, matte;
+uniform sampler2D front;
 uniform float adsk_result_w, adsk_result_h;
 
 void main()
@@ -14,9 +14,8 @@ void main()
 	vec2 iResolution = vec2(adsk_result_w, adsk_result_h);
 	vec2 uv = gl_FragCoord.xy / vec2( adsk_result_w, adsk_result_h);
 	vec3 f = texture2D(front, uv).rgb;
-	vec3 a = texture2D(matte, uv).rgb;
 	
-	gl_FragColor = vec4((f*a), a);
+	gl_FragColor = vec4(f, 1.0);
 }
 	
 	
