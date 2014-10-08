e269d7c3502d35e6b70865772aed3f58c20d780d
kmatchbox <kyle.obley@gmail.com>
2014-10-08 16:40:58 +0200

    Optimized code.

--- K_RgbcmyMatte/K_RgbcmyMatte.glsl
+++ K_RgbcmyMatte/K_RgbcmyMatte.glsl
@@ -1,67 +1,56 @@
 //
-// K_RgbcmyMatte v1.1
+// K_RgbcmyMatte v1.2
 // Shader written by:   Kyle Obley (kyle.obley@gmail.com)
 //
 
 uniform sampler2D image;
 uniform float adsk_result_w, adsk_result_h;
-uniform float whichone;
+uniform int selection;
 
 void main()
 {	
 	vec2 st = gl_FragCoord.xy / vec2 (adsk_result_w, adsk_result_h);
-	
-	int selection = int( floor( whichone ) );
-	
-	vec3 cmy;
-	
-	cmy.x = texture2D(image, st).r;
-	cmy.y = texture2D(image, st).g;
-	cmy.z = texture2D(image, st).b;
-	
-	cmy.x = vec3 (0.0, 0.0, 0.0) + (cmy.x); // Red
-	cmy.y = vec3 (0.0, 0.0, 0.0) + (cmy.y); // Green
-	cmy.z = vec3 (0.0, 0.0, 0.0) + (cmy.z); // Blue
+	vec3 image = texture2D(image, st).rgb;
 	
 	// Red
 	if ( selection == 1 )
 	{
-		gl_FragColor = vec4( cmy.x * (1.0 - cmy.y) * (1.0 - cmy.z) );
+		gl_FragColor = vec4( image.r * (1.0 - image.g) * (1.0 - image.b) );
 	}
 	
 	// Green
 	else if ( selection == 2 )
 	{
-		gl_FragColor = vec4( (1.0 - cmy.x) * cmy.y * (1.0 - cmy.z) );
+		gl_FragColor = vec4( (1.0 - image.r) * image.g * (1.0 - image.b) );
 	}
 	
 	// Blue
 	else if ( selection == 3 )
 	{
-		gl_FragColor = vec4( (1.0 - cmy.x) * (1.0 - cmy.y) * cmy.z );
+		gl_FragColor = vec4( (1.0 - image.r) * (1.0 - image.g) * image.b );
 	}
 	
 	// Cyan
 	else if ( selection == 4 )
 	{
-		gl_FragColor = vec4( (1.0 - cmy.x) * cmy.y * cmy.z );
+		gl_FragColor = vec4( (1.0 - image.r) * image.g * image.b );
 	}
 	
 	// Yellow
 	else if ( selection == 5 )
 	{
-		gl_FragColor = vec4(  cmy.x * cmy.y  * (1.0 - cmy.z) );
+		gl_FragColor = vec4(  image.r * image.g  * (1.0 - image.b) );
 	}
 	
 	// Magenta
 	else if ( selection == 6 )
 	{
-		gl_FragColor = vec4( cmy.z * (1.0 - cmy.x ) * cmy.y );
+		gl_FragColor = vec4( image.b * (1.0 - image.r ) * image.g );
 	}
 	
 	// White
 	else if ( selection == 7 )
 	{
-		gl_FragColor = vec4( cmy.x * cmy.y * cmy.z );
+		gl_FragColor = vec4( image.r * image.g * image.b );
 	}
 }
